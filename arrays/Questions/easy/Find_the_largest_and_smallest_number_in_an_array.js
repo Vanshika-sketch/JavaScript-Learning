3b701cda05c78acebe7097983b31ae8c354b8c9a
Vanshika Sharma <vanshika11082004@gmail.com>
2025-09-17 23:50:56 +0530

    Find the largest and smallest number in an array

--- arrays/Questions/easy/Find_the_largest_and_smallest_number_in_an_array.js
+++ arrays/Questions/easy/Find_the_largest_and_smallest_number_in_an_array.js
@@ -1,2 +1,5 @@
-let numbers = [15,8,25,3,42]
-let largest = Math.max()
+let numbers = [15, 8, 25, 3, 42];
+let largest = Math.max(...numbers);
+let smallest = Math.min(...numbers);
+console.log("Largest:", largest);   // 42
+console.log("Smallest:", smallest); // 3
