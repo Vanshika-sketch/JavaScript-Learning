68c3589d1356c40c72ef6e83172fd1bf0a48e758
Vanshika Sharma <vanshika11082004@gmail.com>
2025-09-23 11:02:57 +0530

    questions

--- Objects/Questions/easy/check_property_existence.js
+++ Objects/Questions/easy/check_property_existence.js
@@ -2,5 +2,18 @@ let student = {name : 'vanshika' , age : 21}
 
 // using 'in' (checks own + inherited peoperties)
 
-console.log('name' in student)
-console.log('toString' in student)
+// console.log('name' in student)
+// console.log('toString' in student)  /*student does not have toString as its own property.BUT — every object in JS inherits from Object.prototype by default, and Object.prototype has a method called toString().*/
+
+// using hasOwnProperty(checks own properties only)
+// console.log(student.hasOwnProperty('age'))
+// console.log(student.hasOwnProperty('toString'))
+
+/*hasOwnProperty checks only the object’s own properties (not the prototype chain). toString is inherited from Object.prototype, not directly defined in student.
+Since hasOwnProperty ignores inherited ones, it returns false.*/
+
+
+//utility finction (safe check)
+function hasProp(obj,prop){
+    return Object.prototype.hasOwnProperty.call(obj,prop)
+}
