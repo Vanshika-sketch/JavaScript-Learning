05d4b97a7d054e0b585606b6cf580609c2342c2f
Vanshika Sharma <vanshika11082004@gmail.com>
2025-09-25 11:57:59 +0530

    Practice for function

---
+++ functions/Practice/practice.js
@@ -0,0 +1,25 @@
+//                        // function declaration
+
+// function greet(){
+//     console.log("good morning!")
+// }
+
+// greet() // calling the function
+
+
+//                       // function with parameters
+
+// function greetUser(name){
+//     console.log("hello " + name + "!")
+// }
+
+// greetUser("roy")
+
+
+//                         FUNCTION WITH RETURN
+
+function add (a,b){
+    return a + b
+}
+let result = add(5,4)
+console.log(result)
