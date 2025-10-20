127b5a2899c686833dfde98f9f583e017aa28cf9
Vanshika Sharma <vanshika11082004@gmail.com>
2025-10-20 20:49:38 +0530

    Counter

---
+++ Practice Project/Counter 2/script.js
@@ -0,0 +1,20 @@
+const countValue = document.getElementById("count")
+const decreaseBtn = document.getElementById("decrease")
+const resetBtn = document.getElementById("reset")
+const increaseBtn = document.getElementById("increase")
+
+let count = 0
+
+increaseBtn.addEventListener("click",function(){
+    count++
+    countValue.textContent =count
+
+})
+decreaseBtn.addEventListener("click",function(){
+    count--;
+    countValue.textContent=count
+})
+resetBtn.addEventListener("click",function(){
+    count = 0
+    countValue.texContent = count
+})
