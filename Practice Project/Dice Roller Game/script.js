ac2f8734fcc72e3417da5a176bb99209487bbbef
Vanshika Sharma <vanshika11082004@gmail.com>
2025-10-30 12:11:20 +0530

    added DOM element selections for dice roller (rollBtn,resultSpan,historyList)

---
+++ Practice Project/Dice Roller Game/script.js
@@ -0,0 +1,5 @@
+// select DOM elements
+
+const rollBtn = document.getElementById("rollBtn")
+const resultSpan = document.getElementById("result")
+const historyList = document.getElementById("history")
