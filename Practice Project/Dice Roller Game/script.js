a0d39f1a2c8caf24a10931802addfddcf2d8b6ee
Vanshika Sharma <vanshika11082004@gmail.com>
2025-11-03 14:38:01 +0530

    added variables and DOM seection in js , wrote dice roll logic(math.random,etc)

--- Practice Project/Dice Roller Game/script.js
+++ Practice Project/Dice Roller Game/script.js
@@ -3,3 +3,20 @@
 const rollBtn = document.getElementById("rollBtn")
 const resultSpan = document.getElementById("result")
 const historyList = document.getElementById("history")
+
+//Function that returns a random integer between min and max(inclusive)
+
+function randomInt(min,max){
+    return Math.floor(Math.random()*(max - min + 1)) + min
+}
+
+// Roll the die and update UI
+function rollDie(){
+    const value = randomInt(1,6)    // generate 1..6
+    resultSpan.textContent = value;    // show latest result
+
+}
+//create  a new list item for history
+
+const li= document.createElement("li")
+li.textContent = `You rolled : $(value)` // set visible text
