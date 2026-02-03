423cabf8ab6073b15c74b4d725113bd7e27b8277
Vanshika Sharma <vanshika11082004@gmail.com>
2026-02-03 13:55:30 +0530

    did basic promise concept

--- Asynchronous/promises.js
+++ Asynchronous/promises.js
@@ -4,3 +4,25 @@ let stocks = {
   Holder :["cone","cup","stick"],
   Toppings :["choclate","peanuts"],
 };
+
+
+let is_shop_open = true;
+
+
+let order = (time,work)=>{
+
+    return new Promise ((resolve,reject)=>{
+        if (is_shop_open){
+            setTimeout(()=>{
+                 resolve(work());
+
+            },time);
+           
+        }
+         else{
+            reject(console.log("our shop is closed"))
+         }
+    })
+}
+
+order(2000,()=>console.log(`${stocks.Fruits[0]} was selected `))
