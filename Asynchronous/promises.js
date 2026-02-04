f931ae749b551a4886fd22692d0be55abaaab35c
Vanshika Sharma <vanshika11082004@gmail.com>
2026-02-04 11:17:18 +0530

    understood the concept of promise

--- Asynchronous/promises.js
+++ Asynchronous/promises.js
@@ -26,3 +26,52 @@ let order = (time,work)=>{
 }
 
 order(2000,()=>console.log(`${stocks.Fruits[0]} was selected `))
+
+.then(()=>{
+    return order(0,()=>console.log("production has started"));
+})
+
+
+
+.then(()=>{
+    return order(2000,()=>console.log("the fruit was chopped"))
+})
+
+
+
+.then(()=>{
+    return order(1000,()=>{
+        console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1] }was selected`)
+    })
+})
+
+.then(()=>{
+    return order(1000,()=>console.log("start the machine"
+
+    ))
+})
+
+.then(()=>{
+    return order(2000,()=>{
+        console.log(`ice cream placed on ${stocks.Holder[0]}`)
+    })
+})
+.then(()=>{
+    return order(3000,()=>{
+        console.log(`${stocks.Toppings[0]} was selected`)
+    })
+})
+.then(()=>{
+    return order(1000,()=>console.log("ice cream was served"))
+})
+
+
+
+.catch(()=>{console.log("customer has left")})
+
+
+
+
+.finally(()=>{
+    console.log("day ended, shop has closed")
+})
